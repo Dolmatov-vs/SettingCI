3990cfae20610563ae39bb211a49c8d5fc05b601
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-07-02 18:16:32 +0300

    fail schema

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -7,19 +7,13 @@ public class NewTest {
 
     @Test
     void shouldReturnDemoAccounts() {
-        // Подход: Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
                 .when()
-// get метод запроса GET
-// URI относительно baseUri
                 .get("/demo/accounts")
-                // Проверки
+
                 .then()
                 .statusCode(200)
-                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
